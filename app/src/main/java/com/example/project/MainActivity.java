4a29906cbe6ac00ebe021759cbbb7a64dc8f797c
ChandanSatyendraPrasad <chandansatyendraprasad@gmail.com>
2020-04-19 16:04:00 +0530

    Base Url

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -7,7 +7,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,22 +18,38 @@ public class MainActivity extends AppCompatActivity {
     RadioButton radlandlord,radtenant;
     TextView textView,textView2,regland,regten;
     EditText edemail,edpass;
-
+    RadioGroup radioGroup;
+    static int flag ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
 
         submit=findViewById(R.id.button);
         regland=findViewById(R.id.textView8);
         regten=findViewById(R.id.textView9);
+        // RadioButton for landlord
         radlandlord=findViewById(R.id.radioButton);
+        // RadioButton for tanent
         radtenant=findViewById(R.id.radioButton2);
         edemail=findViewById(R.id.editText2);
         edpass=findViewById(R.id.editText3);
-
+        radioGroup = findViewById(R.id.landtent);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+               if(radlandlord.isChecked())
+               {
+                   flag =1;
+               }
+               else if (radtenant.isChecked())
+               {
+                   flag=2;
+               }
+            }
+        });
         regland.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -49,8 +67,28 @@ public class MainActivity extends AppCompatActivity {
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this,Nav_tenant.class);
-                startActivity(intent);
+              if(flag!=0)
+              {
+                 switch (flag) {
+                     case 1:
+                         Toast.makeText(MainActivity.this, "Landlord", Toast.LENGTH_SHORT).show();
+                         flag = 0;
+                         radioGroup.clearCheck();
+                         break;
+                     case 2:
+                         Toast.makeText(MainActivity.this, "Tanent", Toast.LENGTH_SHORT).show();
+                         flag = 0;
+                         radioGroup.clearCheck();
+                         break;
+                 }
+              }
+              else
+              {
+                  Toast.makeText(MainActivity.this, "Kindly Select Role", Toast.LENGTH_SHORT).show();
+              }
+
+                //  Intent intent=new Intent(MainActivity.this,Nav_tenant.class);
+              //  startActivity(intent);
 
             }
         });
